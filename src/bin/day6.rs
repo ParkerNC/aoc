a3340d82728a4df7a9cd4e76623c2b1d8ef83fbe
ParkerNC <pcollie4@vols.utk.edu>
2023-12-06 15:54:29 -0500

    day6 start

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -3,15 +3,30 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main(){
-    if let Ok(lines) = read_lines("./dat/input_day5_small.txt") {
+
+    let mut input: Vec<String> = Vec::new();
+    //read in file
+    if let Ok(lines) = read_lines("./dat/input_day6.txt") {
         for line in lines{
             if let Ok(ranges) = line {
-                println!("{ranges}");
+                input.push(ranges);
             }
         }
     }
+    else{
+        println!("bad file");
+    }
+
+    let pairs = make_pairs(input);
 }
 
+fn make_pairs(input: Vec<String>) -> Vec<(i32, i32)>{
+
+    let ret: Vec<(i32, i32)> = Vec::new();
+
+
+    ret
+}
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
